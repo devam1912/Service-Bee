6cb026822a159e6c2697487767d2c07d4529b0f2
Devam <devamtanna07@gmail.com>
2026-03-07 23:41:39 +0530

    Fix: Add TLS ignore for Gmail SMTP on Render

--- Backend/utils/email.js
+++ Backend/utils/email.js
@@ -18,6 +18,9 @@ export const sendEmail = async (to, subject, text, html) => {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
         },
+        tls: {
+            rejectUnauthorized: false
+        },
         connectionTimeout: 10000,
         greetingTimeout: 10000,
         socketTimeout: 10000
